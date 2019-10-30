bce5255dd68ebef98b49e4f6a983479bfdbbb4ac
fuqing <289557905@qq,com>
2019-10-30 15:32:53 +0800

    test websocket

--- apps/hosts/comsumers.py
+++ apps/hosts/comsumers.py
@@ -1,8 +1,10 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from api.utils.ansible_api import ANSRunner
 import json
 
 class AnsibleModel(WebsocketConsumer):
+    '''test websocket sync'''
     def __init__(self, *args, **kwargs):
         super(AnsibleModel, self).__init__(*args, **kwargs)
 
@@ -14,17 +16,23 @@ class AnsibleModel(WebsocketConsumer):
         print ('send_msg:',msg,logId)
 
     def connect(self):
+        '''接受websocket 请求'''
         self.group_name = self.scope['url_route']['kwargs']['group_name']
         print (self.group_name )
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name) #异步使用redis建立组和通道，可以给组发消息
 
-        self.accept()
+        self.accept()   #允许接受请求
 
     def receive(self, text_data=None, bytes_data=None):
-
+        '''获取websocket请求内容'''
         request = json.loads(text_data)
         print (request)
-        self.send(text_data="<font color='#FA8072'> {stdout} </font>".format(stdout=request['custom']))
+        rbt = ANSRunner([], redisKey='1')
+        rbt.run_model(host_list=[request['ip']], module_name='shell',
+                      module_args='{}'.format(request['arg']))
+        data = rbt.get_model_result()
+
+        self.send(text_data="<font color='#FA8072'> {stdout} </font>".format(stdout=json.dumps(data)))
         self.send("\n<font color='red'>执行完成，总共{count}台机器，耗时：{time}</font>".format(count=1, time=2))
 
         self.close()
@@ -35,5 +43,6 @@ class AnsibleModel(WebsocketConsumer):
 
 
     def disconnect(self, close_code):
+        '''异步使用redis离开组和通道，可以给组发消息'''
         async_to_sync(self.channel_layer.group_discard)(self.group_name, self.channel_name)
         self.close()
---
+++ fdommp/asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI entrypoint file for default channel layer.
+
+Points to the channel layer configured as "default" so you can point
+ASGI applications at "databinding.asgi:channel_layer" as their channel layer.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fdommp.settings")
+django.setup()
+application = get_default_application()
